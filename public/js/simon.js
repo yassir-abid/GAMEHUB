8d66a662086b48a97b662d9911b86221c5afea7a
yassir-abid <abid.yassir@gmail.com>
2022-08-22 16:22:59 +0200

    simon says sequence

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,8 +1,11 @@
 const game = {
     colors: ['red', 'green', 'blue', 'yellow'],
 
+    sequence: [],
+
     init: () => {
         game.drawCells();
+        game.newGame();
     },
 
     drawCells: () => {
@@ -14,6 +17,32 @@ const game = {
             cell.style.backgroundColor = color;
             simonDiv.appendChild(cell);
         });
+    },
+
+    newGame: () => {
+        game.sequence = [];
+        for (let index = 0; index < 3; index += 1) {
+            const random = Math.floor(Math.random() * 4);
+            game.sequence.push(game.colors[random]);
+        }
+
+        game.simonSays(game.sequence);
+    },
+
+    simonSays: (sequence) => {
+        if (sequence && sequence.length) {
+            setTimeout(game.bumpCell, 500, sequence[0]);
+            setTimeout(game.simonSays, 850, sequence.slice(1));
+        } else {
+            // player turn
+        }
+    },
+
+    bumpCell: (color) => {
+        document.getElementById(color).style.borderWidth = '45px';
+        setTimeout(() => {
+            document.getElementById(color).style.borderWidth = '0';
+        }, 150);
     },
 };
 
