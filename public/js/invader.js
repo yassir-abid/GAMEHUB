add3428671b17f3d33d32d0249fcc2cf1296816c
yassir-abid <abid.yassir@gmail.com>
2022-08-22 12:37:45 +0200

    handle color change

--- public/js/invader.js
+++ public/js/invader.js
@@ -5,14 +5,31 @@ const game = {
     gridSize: 8,
     cellSize: 30,
 
+    styles: [
+        'plain',
+        'empty',
+        'light',
+        'highlight',
+    ],
+    selectedColor: 'plain',
+
     init: () => {
         game.createGrid();
+        game.initPalette();
     },
 
     createGrid: () => {
+        game.invaderDiv.innerHTML = '';
+
         const table = document.createElement('table');
 
-        game.invaderDiv.innerHTML = '';
+        table.addEventListener('click', (event) => {
+            for (let i = 0; i < game.styles.length; i += 1) {
+                event.target.classList.remove(game.styles[i]);
+            }
+
+            event.target.classList.toggle(game.selectedColor);
+        });
 
         for (let i = 0; i < game.gridSize; i += 1) {
             const tr = document.createElement('tr');
@@ -30,6 +47,19 @@ const game = {
         }
 
         game.invaderDiv.appendChild(table);
+    },
+
+    initPalette: () => {
+        game.styles.forEach((style) => {
+            document.getElementById(`palette__button__${style}`)
+                .addEventListener('click', (event) => {
+                    game.selectedColor = style;
+
+                    document.querySelector('.palette__button--active').classList.remove('palette__button--active');
+
+                    event.target.classList.add('palette__button--active');
+                });
+        });
     },
 };
 
